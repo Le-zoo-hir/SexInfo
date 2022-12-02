96a1a5e324180fd015f441905e9b97b9d917f706
blurp748 <mathistanghe@hotmail.fr>
2022-12-02 05:58:56 +0100

    r

--- src/views/utils/IstItem.vue
+++ src/views/utils/IstItem.vue
@@ -4,12 +4,12 @@
         <h1 class="text-6xl xl:m-14 mt-14 mb-14 font-bold text-[#00102F]">{{ item.nom }}</h1>
         <div>
             <div class="flex flex-col xl:flex-row items-center">
-                <img src="assets/information-circle-outline.svg" class="w-14 mb-10 xl:mr-10 xl:mb-0">
+                <img src="/assets/information-circle-outline.svg" class="w-14 mb-10 xl:mr-10 xl:mb-0">
                 <p class="font-semibold text-[#00102F] pl-14 pr-14 xl:p-5 text-justify xl:text-start">{{ item.desc }}
                 </p>
             </div>
             <div class="flex flex-col xl:flex-row mt-10 items-center">
-                <img src="assets/shield-outline.svg" class="w-14 mb-10 xl:mr-10 xl:mb-0">
+                <img src="/assets/shield-outline.svg" class="w-14 mb-10 xl:mr-10 xl:mb-0">
                 <div class="flex flex-row flex-wrap justify-around gap-5">
                     <div v-for="oneProtect in item.Protection"
                         class="w-32 h-32 bg-[#e6e6e6] flex flex-col justify-center items-center rounded-xl xl:mr-6">
